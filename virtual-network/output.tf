4a5a7bde68d607ca5bcf2f24df2ff3071edaac41
Krishnadhas N K <108367225+githubofkrishnadhas@users.noreply.github.com>
2024-09-21 02:39:59 +0530

    DEVOPS-279 terraform module for azure vnet init commit (#9)
    * DEVOPS-279 terraform module for azure vnet init commit
    
    * DEVOPS-279 fix typos
    
    * DEVOPS-279 fix rule isssue bya dding depends on
    
    * fix nsg rule issue clash DEVOPS-279

---
+++ virtual-network/output.tf
@@ -0,0 +1,30 @@
+output "resource_group_name" {
+  value       = azurerm_resource_group.rg.name
+  description = "Azure resource group name"
+}
+
+output "location" {
+  description = "Azure Location"
+  value       = azurerm_resource_group.rg.location
+}
+
+output "vnet_name" {
+  description = "Azure Virtual network name"
+  value       = azurerm_virtual_network.vnet.name
+}
+
+output "vnet_address_range" {
+  description = "Azure Vnet address range"
+  value       = azurerm_virtual_network.vnet.address_space
+}
+
+output "subnet_ids" {
+  description = "List of all subnet IDs"
+  value       = azurerm_subnet.subnet[*].id # Collect all subnet IDs
+}
+
+output "network_security_group_id" {
+  description = "Azure NSG ID"
+  value       = azurerm_network_security_group.nsg.id
+}
+
---
+++ virtual-network/variables.tf
@@ -0,0 +1,54 @@
+variable "resource_group_name" {
+  default     = ""
+  description = "Azure Vnet resource group name"
+  type        = string
+}
+
+variable "vnet_name" {
+  default     = ""
+  description = "Azure Vnet name"
+  type        = string
+}
+
+variable "location" {
+  default     = ""
+  description = "Azure location"
+  type        = string
+}
+
+variable "application_name" {
+  default     = ""
+  description = "Azure application name tag"
+  type        = string
+}
+
+variable "environment" {
+  default     = ""
+  description = "Environment tag value in Azure"
+  type        = string
+  validation {
+    condition     = contains(["DEV", "QA", "UAT", "PROD"], var.environment)
+    error_message = "Environment value should be one among DEV or QA or UAT or PROD."
+  }
+}
+
+variable "vnet_address_space" {
+  description = "Azure VNET address space"
+  type        = list(string)
+  default     = []
+  validation {
+    condition     = length(var.vnet_address_space) > 0
+    error_message = "The address_space variable must contain at least one CIDR block."
+  }
+}
+
+variable "subnet_cidrs" {
+  default     = []
+  type        = list(string)
+  description = "Azure Subnet Ip addresses"
+  validation {
+    condition     = length(var.subnet_cidrs) > 0
+    error_message = "Atleast a single subnet CIDR is required."
+  }
+}
+
