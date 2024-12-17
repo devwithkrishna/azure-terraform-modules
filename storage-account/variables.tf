a11b102184b32a18d36ea780af0320a34c83ed0c
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-12-18 00:56:22 +0530

    DEVOPS-289 did terraform fmt

--- storage-account/variables.tf
+++ storage-account/variables.tf
@@ -1,56 +1,68 @@
 variable "resource_group_name" {
-  type = string
+  type        = string
   description = "Azure Storage Account Rg"
 }
 
 variable "location" {
-  type = string
+  type        = string
   description = "Azure storage account location"
-  default = ""
+  default     = ""
 }
 
 variable "storage_account_name" {
   description = "Azure Storage Account name"
-  type = string
+  type        = string
 
 }
 
 variable "account_tier" {
-  default = "Standard"
+  default     = "Standard"
   description = "Tier to use for this storage account. Valid options are Standard and Premium"
 }
 
 variable "account_kind" {
-  default = "StorageV2"
+  default     = "StorageV2"
   description = "Kind of account. Valid options are BlobStorage, BlockBlobStorage, FileStorage, Storage and StorageV2"
 }
 
 variable "account_replication_type" {
   description = "type of replication to use for this storage account. Valid options are LRS, GRS, RAGRS, ZRS, GZRS and RAGZRS"
-  default = "LRS"
+  default     = "LRS"
 }
 
 variable "cross_tenant_replication_enabled" {
-  default = false
+  default     = false
   description = "Should cross Tenant replication be enabled"
 }
 
 variable "public_network_access_enabled" {
-  default = true
+  default     = true
   description = "Whether the public network access is enabled"
 }
 
 variable "delete_retention_policy" {
-  default = 10
+  default     = 10
   description = "Specifies the number of days that the blob should be retained"
 }
 
 variable "environment" {
-  default = "DEV"
+  default     = "DEV"
   description = "Environment tag value in Azure"
 }
 
 variable "application_name" {
-  default = "devwithkrishna"
+  default     = "devwithkrishna"
   description = "Azure application name tag"
+}
+
+
+variable "temporary" {
+  default     = "TRUE"
+  description = "Temporary tag value in Azure"
+  type        = string
+  validation {
+    condition     = contains(["TRUE", "FALSE"], upper(var.temporary))
+    error_message = "The temporary tag value must be either 'TRUE' or 'FALSE'."
+  }
+
 }
