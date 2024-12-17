dd3ecaf84ebaf27f062400fbd8d1e45b83af213d
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-12-18 00:54:43 +0530

    DEVOPS-289 added tags tologanalytics ws module and did terraform fmt

--- log-analytics-workspace/loganalytics.tf
+++ log-analytics-workspace/loganalytics.tf
@@ -6,13 +6,21 @@ resource "azurerm_resource_group" "rg" {
     Orchestrator    = "Terraform"
     DisplayName     = upper(var.resource_group_name)
     ApplicationName = lower(var.application_name)
+    Temporary = upper(var.temporary)
   }
 }
 
 resource "azurerm_log_analytics_workspace" "loganalytics_ws" {
-  name = upper(var.loganalytics_workspace_name)
-  sku = var.loganalytics_sku
+  name                = upper(var.loganalytics_workspace_name)
+  sku                 = var.loganalytics_sku
   resource_group_name = azurerm_resource_group.rg.name
-  location = var.location
-  retention_in_days = var.loganalytics_retention_period
+  location            = var.location
+  retention_in_days   = var.loganalytics_retention_period
+  tags = {
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.resource_group_name)
+    ApplicationName = lower(var.application_name)
+    Temporary = upper(var.temporary)
+  }
 }
