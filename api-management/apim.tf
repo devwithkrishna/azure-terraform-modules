7af89b170835c645ee5f85b8c0e7d059ebfe1035
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-12-18 00:54:09 +0530

    DEVOPS-289 added tags to API management module and did terraform fmt

--- api-management/apim.tf
+++ api-management/apim.tf
@@ -1,6 +1,13 @@
 resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
+  tags = {
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.resource_group_name)
+    ApplicationName = lower(var.application_name)
+    Temporary = upper(var.temporary)
+  }
 }
 
 resource "azurerm_api_management" "apim" {
@@ -9,6 +16,12 @@ resource "azurerm_api_management" "apim" {
   resource_group_name = azurerm_resource_group.rg.name
   publisher_name      = tostring(var.publisher_name)
   publisher_email     = tostring(var.publisher_email)
-
   sku_name = "${var.sku_name_part1}_${var.sku_name_part2}"
+  tags = {
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.resource_group_name)
+    ApplicationName = lower(var.application_name)
+    Temporary = upper(var.temporary)
+  }
 }
