93f8de44840504469cc2eeb98137d8a7b0ac3c0a
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-12-18 00:56:05 +0530

    DEVOPS-289 added tags to storage accnt module and added temporarry and did terraform fmt

--- storage-account/storageaccount.tf
+++ storage-account/storageaccount.tf
@@ -2,22 +2,24 @@ resource "azurerm_resource_group" "storage_rg" {
   name     = var.resource_group_name
   location = var.location
   tags = {
-    Environment = upper(var.environment)
-    Orchestrator = "Terraform"
-    DisplayName = upper(var.resource_group_name)
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.resource_group_name)
     ApplicationName = lower(var.application_name)
+    Temporary       = upper(var.temporary)
+
   }
 }
 
 resource "azurerm_storage_account" "storage" {
-  name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.storage_rg.name
-  location                 = azurerm_resource_group.storage_rg.location
-  account_tier             = var.account_tier
-  account_replication_type = var.account_replication_type
-  account_kind = var.account_kind
+  name                             = var.storage_account_name
+  resource_group_name              = azurerm_resource_group.storage_rg.name
+  location                         = azurerm_resource_group.storage_rg.location
+  account_tier                     = var.account_tier
+  account_replication_type         = var.account_replication_type
+  account_kind                     = var.account_kind
   cross_tenant_replication_enabled = var.cross_tenant_replication_enabled
-  public_network_access_enabled = var.public_network_access_enabled
+  public_network_access_enabled    = var.public_network_access_enabled
 
   blob_properties {
     delete_retention_policy {
@@ -26,9 +28,11 @@ resource "azurerm_storage_account" "storage" {
   }
 
   tags = {
-    Environment = upper(var.environment)
-    Orchestrator = "Terraform"
-    DisplayName = upper(var.storage_account_name)
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.storage_account_name)
     ApplicationName = lower(var.application_name)
+    Temporary       = upper(var.temporary)
+
   }
 }
