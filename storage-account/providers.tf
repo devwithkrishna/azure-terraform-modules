7188bf6d9cde93aa83fecf5177c8893e3056b9ed
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-12-18 00:55:34 +0530

    DEVOPS-289 updated provider version to v4.0 azurerm

--- storage-account/providers.tf
+++ storage-account/providers.tf
@@ -3,13 +3,13 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0"
+      version = "<= 4.0"
     }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.1"
     }
-  } 
+  }
 }
 provider "azurerm" {
   features {}
