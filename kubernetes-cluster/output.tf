a568f37a5905879387199f9d0b6304f54e82ccdb
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2024-12-27 19:50:42 +0530

    DEVOPS-300 output tf code

---
+++ kubernetes-cluster/output.tf
@@ -0,0 +1,9 @@
+output "aks_cluster_name" {
+  description = "Azure AKS name"
+  value       = azurerm_kubernetes_cluster.aks_cluster.name
+}
+
+output "aks_cluster_control_plane_url" {
+  description = "FQDN of the Azure Kubernetes Managed Cluster"
+  value = azurerm_kubernetes_cluster.aks_cluster.fqdn
+}
