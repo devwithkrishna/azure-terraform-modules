742b7f86479461c01e8ba3d90b7ff3c6e9d4183f
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2025-03-20 00:15:48 +0530

    vm fix

--- linux-virtual-machine/vm.tf
+++ linux-virtual-machine/vm.tf
@@ -54,7 +54,7 @@ resource "azurerm_public_ip" "public_ip" {
   name                = var.public_ip_address_name
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Dynamic"
+  allocation_method   = "Static"
 
   tags = {
     Environment     = upper(var.environment)
