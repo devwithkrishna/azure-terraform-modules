3c106cacf1b5d3dc6f9c6d9d91f5b0b81afea6bc
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2025-04-14 20:10:39 +0530

    DEVOPS-315 azure datafactory terraform module

---
+++ data-factory/datafactory.tf
@@ -0,0 +1,29 @@
+resource "azurerm_resource_group" "data_factory_rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.resource_group_name)
+    ApplicationName = lower(var.application_name)
+    Temporary       = upper(var.temporary)
+  }
+}
+
+resource "azurerm_data_factory" "data_factory" {
+  name                   = var.data_factory_name
+  resource_group_name    = azurerm_resource_group.data_factory_rg.name
+  location               = azurerm_resource_group.keyvault_rg.location
+  public_network_enabled = var.public_network_enabled
+
+  managed_virtual_network_enabled = var.managed_virtual_network_enabled
+
+  tags = {
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.data_factory_name)
+    ApplicationName = lower(var.application_name)
+    Temporary       = upper(var.temporary)
+
+  }
+}
