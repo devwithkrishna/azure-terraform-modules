78ad198128c52998175f875bb7b0ba2da64166cf
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2025-04-14 20:34:58 +0530

    fix typo in rg name ref in data factory creation

--- data-factory/datafactory.tf
+++ data-factory/datafactory.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "data_factory_rg" {
 resource "azurerm_data_factory" "data_factory" {
   name                   = var.data_factory_name
   resource_group_name    = azurerm_resource_group.data_factory_rg.name
-  location               = azurerm_resource_group.keyvault_rg.location
+  location               = azurerm_resource_group.data_factory_rg.location
   public_network_enabled = var.public_network_enabled
 
   managed_virtual_network_enabled = var.managed_virtual_network_enabled
