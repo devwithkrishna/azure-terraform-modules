a72a4cb581e34dfa6bcb1009aa99e9c90fd39c0e
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2025-04-18 23:28:47 +0530

    fix mongo_password issue

--- cosmosdb-mongo-vcore/credentials.tf
+++ cosmosdb-mongo-vcore/credentials.tf
@@ -2,7 +2,5 @@ resource "random_password" "password" {
   length           = 32
   min_lower        = 4
   min_numeric      = 4
-  min_special      = 4
   min_upper        = 4
-  override_special = "!#$%&*-_=+[]{}:?"
 }
