cefc67ae197684596cf27236a889757475baacdf
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2025-04-18 23:41:51 +0530

    Revert "fix mongo_password issue"
    his reverts commit a72a4cb581e34dfa6bcb1009aa99e9c90fd39c0e.

--- cosmosdb-mongo-vcore/credentials.tf
+++ cosmosdb-mongo-vcore/credentials.tf
@@ -2,5 +2,7 @@ resource "random_password" "password" {
   length           = 32
   min_lower        = 4
   min_numeric      = 4
+  min_special      = 4
   min_upper        = 4
+  override_special = "!#$%&*-_=+[]{}:?"
 }
