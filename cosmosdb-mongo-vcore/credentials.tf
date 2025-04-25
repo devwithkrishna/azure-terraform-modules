34daa9af2088f0693acb00c7e8af34084e1f98da
Krishnadhas N K <108367225+githubofkrishnadhas@users.noreply.github.com>
2025-04-25 22:20:31 +0530

    terraform fmt for cosmosdb module (#28)

--- cosmosdb-mongo-vcore/credentials.tf
+++ cosmosdb-mongo-vcore/credentials.tf
@@ -1,6 +1,6 @@
 resource "random_password" "password" {
-  length           = 32
-  min_lower        = 4
-  min_numeric      = 4
-  min_upper        = 4
+  length      = 32
+  min_lower   = 4
+  min_numeric = 4
+  min_upper   = 4
 }
