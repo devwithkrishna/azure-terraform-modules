45d03d03ed0aa073dfd333053b1a083c5f2ba0fa
githubofkrishnadhas <krishnadhasnk1997@gmail.com>
2025-04-29 23:26:41 +0530

    DEVOPS-332 terraform module for DNs zone

---
+++ dns-zone/dnszone.tf
@@ -0,0 +1,27 @@
+resource "azurerm_resource_group" "dns_rg" {
+  name     = var.resource_group_name
+  location = var.location
+  tags = {
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.resource_group_name)
+    ApplicationName = lower(var.application_name)
+    Temporary       = upper(var.temporary)
+  }
+}
+
+resource "azurerm_dns_zone" "dns" {
+  name                = var.dns_zone_name
+  resource_group_name = azurerm_resource_group.mongo_rg.name
+
+  tags = {
+    Environment     = upper(var.environment)
+    Orchestrator    = "Terraform"
+    DisplayName     = upper(var.dns_zone_name)
+    ApplicationName = lower(var.application_name)
+    Temporary       = upper(var.temporary)
+
+  }
+
+  depends_on = [azurerm_resource_group.dns_rg]
+}
---
+++ dns-zone/output.tf
@@ -0,0 +1,26 @@
+output "resource_group" {
+  description = "Azure DNS Zone resource group name"
+  value       = azurerm_resource_group.dns_rg.name
+}
+
+output "dns_zone_name" {
+  description = "Azure DNS zone name"
+  value       = azurerm_dns_zone.dns.name
+}
+
+output "resource_group_location" {
+  description = "Azure resource group location"
+  value       = azurerm_resource_group.dns_rg.location
+}
+
+output "dns_id" {
+  description = "DNS zone ID"
+  value       = azurerm_dns_zone.dns.i
+
+}
+
+output "name_servers" {
+  description = "Name servers for the DNS zone"
+  value       = azurerm_dns_zone.dns.name_servers
+
+}
